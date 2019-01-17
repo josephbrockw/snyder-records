035205c5010681dba9637fdcbd52d0c4aa87a5ac
Joe Wilkinson <me@thejoewilkinson.com>
2019-01-16 19:08:01 -0600

    first commit

--- js/custom/app.js
+++ js/custom/app.js
@@ -1,53 +1,83 @@
 $(document).ready(function() {
+  var $window = $(window);
 
+  function checkWidth() {
+    var windowSize = $window.width()
+    if(windowSize >= 1060) {
+      // $(".main-menu").removeClass("dropdown");
+      // $(".main-menu").css("display", "inline-block");
+      // $(".menu-icon").css("display", "none");
+    } else {
+      // $(".main-menu").addClass("dropdown");
+      $(".dropdown").addClass("hidden");
+      // $(".menu-icon").css("display", "inline-block");
+
+    }
+  }
+  $window.resize(checkWidth())
   // Dropdown Menu for mobile
   $(".menu-icon").click(function(){
-    if( $(".main-menu").hasClass("dropdown")) {
-      // If menu is visible, hide
-      $(".main-menu").removeClass("dropdown");
-      $(".main-menu").css("display", "none");
+    console.log('click');
+    if( $(".dropdown").hasClass("hidden")) {
+      // show menu
+      $(".dropdown").removeClass("hidden");
+      $(".dropdown").css("right", "0");
     } else {
-      // Else, show menu
-      $(".main-menu").addClass("dropdown");
-      $(".main-menu").css("display", "inline-block");
-      console.log("click");
+      // Else, hide menu
+      $(".dropdown").addClass("hidden");
+      $(".dropdown").css("right", "-18rem");
     }
   });
 
-  // Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
+  $num = $('.my-card').length;
+  $even = $num / 2;
+  $odd = ($num + 1) / 2;
+
+  if ($num % 2 == 0) {
+    $('.my-card:nth-child(' + $even + ')').addClass('active');
+    $('.my-card:nth-child(' + $even + ')').prev().addClass('prev');
+    $('.my-card:nth-child(' + $even + ')').next().addClass('next');
+  } else {
+    $('.my-card:nth-child(' + $odd + ')').addClass('active');
+    $('.my-card:nth-child(' + $odd + ')').prev().addClass('prev');
+    $('.my-card:nth-child(' + $odd + ')').next().addClass('next');
+  }
+
+  $('.my-card').click(function() {
+    $slide = $('.active').width();
+    console.log($('.active').position().left);
+
+    if ($(this).hasClass('next')) {
+      $('.card-carousel').stop(false, true).animate({left: '-=' + $slide});
+    } else if ($(this).hasClass('prev')) {
+      $('.card-carousel').stop(false, true).animate({left: '+=' + $slide});
+    }
+
+    $(this).removeClass('prev next');
+    $(this).siblings().removeClass('prev active next');
+
+    $(this).addClass('active');
+    $(this).prev().addClass('prev');
+    $(this).next().addClass('next');
+  });
+
+
+  // Keyboard nav
+  $('html body').keydown(function(e) {
+    if (e.keyCode == 37) { // left
+      $('.active').prev().trigger('click');
+    }
+    else if (e.keyCode == 39) { // right
+      $('.active').next().trigger('click');
     }
   });
+
+  $('.left-arrow').click(function() {
+    $('.active').prev().trigger('click');
+  });
+
+  $('.right-arrow').click(function() {
+    $('.active').next().trigger('click');
+  });
+
 });
